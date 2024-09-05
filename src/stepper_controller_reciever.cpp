1564aaae18a90ea8bfb8005683f1691a03e56a7b
niitfa <121084020+niitfa@users.noreply.github.com>
2024-09-05 13:35:20 +0300

    ignores

--- src/stepper_controller_reciever.cpp
+++ src/stepper_controller_reciever.cpp
@@ -14,6 +14,7 @@
 #include <string.h>
 #include <string>
 #include <netinet/in.h>
+#include <fcntl.h>
 #endif
 
 #include "stepper_controller_reciever.h"
@@ -75,8 +76,7 @@ int32_t StepperControllerReciever::GetAngEncoderValue()
 
 void StepperControllerReciever::ThreadHandler()
 {
-#ifdef _WIN32
-	/* ���������������� */
+#ifdef _WIN32 
 	WSAData wsaData;
 	WORD DllVersion = MAKEWORD(2, 1);
 	if (WSAStartup(DllVersion, &wsaData) != 0) {
@@ -89,21 +89,6 @@ void StepperControllerReciever::ThreadHandler()
 	addr.sin_port = htons(this->port);
 	addr.sin_family = AF_INET;
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, NULL);
-#endif
-#ifdef __linux__
-	sockaddr_in hint;
-	memset(&hint, 0, sizeof(hint));
-	hint.sin_family = AF_INET;
-	inet_pton(AF_INET, this->ip.c_str(), &hint.sin_addr.s_addr);
-	//hint.sin_addr.s_addr = htonl(INADDR_ANY);
-	hint.sin_port = this->port;
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if(sock < 0)
-	{
-		std::cout << "failed to create client socket\n";
-	}
-	//socklen_t len = sizeof(hint);
-#endif
 
 	std::vector<char> message_vec;
 
@@ -119,7 +104,9 @@ void StepperControllerReciever::ThreadHandler()
 	this->initialized.store(true);
 	while (!this->stopped.load())
 	{
+		std::cout << "reading... \n";
 		int recv_size = recv(sock, buff, kBufferSize, 0);
+		std::cout << "recv_size: " << recv_size << std::endl;
 		if (recv_size > 0)
 		{
 			bytes_received += recv_size;
@@ -149,34 +136,98 @@ void StepperControllerReciever::ThreadHandler()
 		else
 		{
 			memset(this->message, 0, kMessageSize);
-
-#ifdef _WIN32
 			closesocket(sock);
-#endif
-#ifdef __linux__
-			close(sock);
-#endif
 			sock = socket(AF_INET, SOCK_STREAM, 0);
-#ifdef _WIN32
 			if (connect(sock, (SOCKADDR*)&addr, addrLen) < 0)
-#endif
-#ifdef __linux__
-			if (connect(sock, (sockaddr*)&hint, sizeof(hint) < 0))
-#endif
 			{
-				// Connection failed
-				std::cout << "Connection failed\n";
+				// Conn error
 			}
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 		}
 	}
 	memset(this->message, 0, kMessageSize);
-#ifdef _WIN32
-		closesocket(sock);
+	closesocket(sock);
+	this->started.store(false);
 #endif
 
+
+
+
+
 #ifdef __linux__
+	sockaddr_in hint;
+	memset(&hint, 0, sizeof(hint));
+	inet_pton(AF_INET, this->ip.c_str(), &hint.sin_addr);
+	hint.sin_port = htons(this->port);
+	hint.sin_family = AF_INET;	
+
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	fcntl(sock, F_SETFL, O_NONBLOCK);
+	//socklen_t len = sizeof(hint);
+
+	std::vector<char> message_vec;
+
+	if (sock == -1)
+	{
+		return;
+	}
+
+	int bytes_received = 0;
+	char buff[kBufferSize];
+	memset(buff, 0, kBufferSize);
+
+	this->initialized.store(true);
+
+	while(!this->stopped.load())
+	{
+		int conn = connect(sock, (const sockaddr*)&hint, sizeof(hint));
+		if(conn < 0)
+		{
+			//std::cout << "connection failed\n";
+			memset(buff, 0, kBufferSize);
+			memset(message, 0, kMessageSize);
+			message_vec.clear();
+
+		}
+		else
+		{
+			//std::cout << "connection established!\n";
+			int recv_size = recv(sock, buff, kBufferSize, 0);
+			if (recv_size > 0)
+			{
+				bytes_received += recv_size;
+				for (int i = 0; i < recv_size; ++i)
+				{
+					message_vec.push_back(buff[i]);
+				}
+
+				if (bytes_received == kMessageSize)
+				{
+					// Make user copy of received message
+					// Copy from vec to buff
+					for (int i = 0; i < message_vec.size(); ++i)
+					{
+						buff[i] = message_vec.at(i);
+					}
+					this->mtx.lock();
+					memcpy(this->message, buff, kMessageSize);
+					this->mtx.unlock();
+
+					// Clean buffer and vector
+					memset(buff, 0, kBufferSize);
+					message_vec.clear();
+					bytes_received = 0;
+				}
+			}
+
+		}
+
 		close(sock);
-#endif
+		sock = socket(AF_INET, SOCK_STREAM, 0);
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+	memset(this->message, 0, kMessageSize);
+	close(sock);
 	this->started.store(false);
+#endif
 }
